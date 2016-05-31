f9c36b6d4967871d338c6a5d106797fba4962574
Jeremy Scheff <jdscheff@gmail.com>
2016-05-30 20:05:35 -0400

    Minify CSS

--- tools/build.sh
+++ tools/build.sh
@@ -1,5 +1,9 @@
 rm -rf build/*
 cp src/index.html build
+
 cp -r src/css build/css
+rm build/css/*.css
+cat src/css/*.css | cleancss -o build/css/app.css
+
 NODE_ENV=production browserify src/app.js > build/app.js
 NODE_ENV=production browserify src/worker.js > build/worker.js
